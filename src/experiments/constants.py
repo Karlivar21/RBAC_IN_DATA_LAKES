5dd7d71d64ef72f4486817efcd80ff04dbebbcd2
Rasmus Herskind <47327266+ramuld12@users.noreply.github.com>
2025-12-07 18:08:37 +0100

    Experiment 1 now properly done

--- src/experiments/constants.py
+++ src/experiments/constants.py
@@ -3,7 +3,8 @@
 BUCKET_NAME = "s3-rbac-in-data-lakes-experiments"
 ACCOUNT_ID = "501994300007" 
 REGION = "eu-north-1"
-EMPLOYEE_DATA_KEY = "employee_data_encrypted.parquet"
+EMPLOYEE_DATA_RAW_KEY = "employee_data_raw.parquet"
+EMPLOYEE_DATA_ENCRYPTED_KEY = "employee_data_encrypted.parquet"
 
 # Encrypted column KMS Key IDs
 SALARY_KEY_ID = "86c41c3f-fc21-4730-a20b-b755e5b63ebb"
--- src/experiments/utility_functions.py
+++ src/experiments/utility_functions.py
@@ -1,14 +1,9 @@
 # Imports
 import io
-import boto3
-import pandas as pd
-import pyarrow.parquet as pq
-import pyarrow as pa
-from cryptography.fernet import Fernet
 from io import BytesIO
-from dotenv import load_dotenv
 import os
 from constants import *
+import pandas as pd
 
 def list_s3_bucket_objects(s3_client):
     response = s3_client.list_objects_v2(Bucket=BUCKET_NAME)
@@ -26,19 +21,4 @@ def retrieve_data(s3_client, file_name):
     df = pd.read_parquet(data, engine="pyarrow")
     print(f"\n successfully loaded '{file_name}' into DataFrame.")
     return df
-
-
-def encrypt_column_in_parquet(df, column_to_encrypt, fernet_key):
-    """Encrypt a specific column in a Parquet file and upload the result to S3."""
-    fernet = Fernet(fernet_key)
-    print(f"Encrypting column: {column_to_encrypt}")
-    if column_to_encrypt in df.columns:
-        df[column_to_encrypt] = df[column_to_encrypt].astype(str).apply(
-            lambda x: fernet.encrypt(x.encode()).decode()
-        )
-    else:
-        raise ValueError(f"Column '{column_to_encrypt}' not found in Parquet file")
-    return df
-
-
 
